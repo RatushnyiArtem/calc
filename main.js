ce610716d710981bd884bd94c2122b4c9e5f3466
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2022-01-16 16:19:37 +0100

    calc

---
+++ main.js
@@ -0,0 +1,37 @@
+let a = "привет я хочу изучать js " + "после изучения которого, я буду крутым программистом"
+alert(a)
+alert('Давай найдём сумму чисел')
+let b = Number(prompt('Какое число первое?'))
+let c = Number(prompt('Второе число?'))
+let d;
+d = b+c;
+alert(d);
+console.log(d);
+alert('Давай вычтем число')
+let e = Number(prompt('Какое число первое?'))
+let f = Number(prompt('Второе число?'))
+let g;
+g = e-f;
+alert(g);
+console.log(g);
+alert('Давай умножим число')
+let h = Number(prompt('Какое число первое?'))
+let i = Number(prompt('Второе число?'))
+let j;
+j = h*i;
+alert(j);
+console.log(j);
+alert('Давай поделим число')
+let k = Number(prompt('Какое число первое?'))
+let l = Number(prompt('Второе число?'))
+let m;
+m = k/l;
+alert(m);
+console.log(m);
+alert('Давай возведём в степень число')
+let n = Number(prompt('Какое число первое?'))
+let o = Number(prompt('Второе число?'))
+let p;
+p = n**o;
+alert(p);
+console.log(p);
